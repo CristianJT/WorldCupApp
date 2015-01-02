aefa0e2bc5af34f0c61890164cc65ba03713111d
CristianJT <cjtodorovic@outlook.com>
2015-01-02 19:23:04 -0300

    Beginning the API REST: Country.cs and Player.cs

---
+++ WorldCup/WorldCup/Models/Country.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorldCup.Models
+{
+    public class Country
+    {
+        public int CountryId { get; set; }
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public int Seed { get; set; }
+        public int Cups { get; set; }
+    }
+}
---
+++ WorldCup/WorldCup/Models/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WorldCup.Models
+{
+    public class Player
+    {
+        public int PlayerId { get; set; }
+        public string Name { get; set; }
+        public DateTime BornDate { get; set; }
+        public string Position { get; set; }
+        public int Number { get; set; }
+        public int CountryId { get; set; }
+        [ForeignKey("CountryId")]
+        public Country Team { get; set; }
+    }
+}
